2c7f669987b3ada237366abb13df0f3cb157f6b9
djekispich-debug <djekispich@gmail.com>
2026-06-13 14:15:43 +0300

    Update VVisualsClient.kt

--- src/client/kotlin/com/example/client/VVisualsClient.kt
+++ src/client/kotlin/com/example/client/VVisualsClient.kt
@@ -4,31 +4,34 @@ import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.DrawContext
+import org.slf4j.LoggerFactory
 
 class VVisualsClient : ClientModInitializer {
     
+    private val logger = LoggerFactory.getLogger("VVisuals")
+    
     override fun onInitializeClient() {
-        // Регистрируем кастомный HUD
-        HudRenderCallback.EVENT.register { drawContext, tickDelta ->
-            renderFpsHud(drawContext)
+        logger.info("VVisuals Client инициализирован! Мод работает!")
+        
+        HudRenderCallback.EVENT.register { context, tickDelta ->
+            renderFpsHud(context)
         }
     }
     
     private fun renderFpsHud(context: DrawContext) {
         val client = MinecraftClient.getInstance()
-        val textRenderer = client.textRenderer
+        val textRenderer = client.textRenderer ?: return
         
         val fps = client.currentFps
-        val fpsText = "FPS: $fps"
+        val fpsText = "§aFPS: §f$fps"
         
-        // Рисуем текст в левом верхнем углу
         context.drawText(
             textRenderer,
             fpsText,
-            5,  // x
-            5,  // y
-            0xFFFFFF, // белый цвет
-            true // shadow
+            5,
+            5,
+            0xFFFFFF,
+            true
         )
     }
 }
