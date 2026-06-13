5e7ed1aed52e7594bf37416790c44b31d886b7be
djekispich-debug <djekispich@gmail.com>
2026-06-13 14:06:50 +0300

    Update VVisualsClient.kt

--- src/client/kotlin/com/example/client/VVisualsClient.kt
+++ src/client/kotlin/com/example/client/VVisualsClient.kt
@@ -1,9 +1,34 @@
 package com.example.client
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.gui.DrawContext
 
-object VVisualsClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+class VVisualsClient : ClientModInitializer {
+    
+    override fun onInitializeClient() {
+        // Регистрируем кастомный HUD
+        HudRenderCallback.EVENT.register { drawContext, tickDelta ->
+            renderFpsHud(drawContext)
+        }
+    }
+    
+    private fun renderFpsHud(context: DrawContext) {
+        val client = MinecraftClient.getInstance()
+        val textRenderer = client.textRenderer
+        
+        val fps = client.currentFps
+        val fpsText = "FPS: $fps"
+        
+        // Рисуем текст в левом верхнем углу
+        context.drawText(
+            textRenderer,
+            fpsText,
+            5,  // x
+            5,  // y
+            0xFFFFFF, // белый цвет
+            true // shadow
+        )
+    }
 }
